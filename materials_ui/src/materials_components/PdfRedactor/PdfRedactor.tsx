b9293762e7fc018d88cb4fdbaac9013a52bf14e1
robmolloy-cps <robert.molloy@cps.gov.uk>
2026-04-27 13:40:19 +0000

    change-pdf-viewer-height-to-80vh

--- materials_ui/src/materials_components/PdfRedactor/PdfRedactor.tsx
+++ materials_ui/src/materials_components/PdfRedactor/PdfRedactor.tsx
@@ -442,7 +442,7 @@ export const PdfRedactor = (p: {
           ref={containerRef}
           style={{
             position: 'relative',
-            height: '700px',
+            height: '80vh',
             width: '100%',
             overflowX: 'scroll',
             overflowY: 'scroll',
