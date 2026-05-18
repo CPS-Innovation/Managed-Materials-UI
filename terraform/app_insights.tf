6c189b0013ac231efae2d4bc2ab51f976c973405
Daniel Barber <daniel.barber@cps.gov.uk>
2026-05-18 15:55:10 +0100

    Removing existing materials logging resources

--- terraform/app_insights.tf
+++ terraform/app_insights.tf
@@ -1,17 +1,18 @@
-resource "azurerm_log_analytics_workspace" "law_materials" {
-  name                = "la-${local.resource_name}"
-  location            = azurerm_resource_group.rg_materials.location
-  resource_group_name = azurerm_resource_group.rg_materials.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
+# Logging is set to point to the existing polaris resources
+# resource "azurerm_log_analytics_workspace" "law_materials" {
+#   name                = "la-${local.resource_name}"
+#   location            = azurerm_resource_group.rg_materials.location
+#   resource_group_name = azurerm_resource_group.rg_materials.name
+#   sku                 = "PerGB2018"
+#   retention_in_days   = 30
+# }
 
-resource "azurerm_application_insights" "ai_materials" {
-  name                = "ai-${local.resource_name}"
-  location            = azurerm_resource_group.rg_materials.location
-  resource_group_name = azurerm_resource_group.rg_materials.name
-  application_type    = "web"
-  workspace_id        = azurerm_log_analytics_workspace.law_materials.id
-  retention_in_days   = 30
-  tags                = local.common_tags
-}
+# resource "azurerm_application_insights" "ai_materials" {
+#   name                = "ai-${local.resource_name}"
+#   location            = azurerm_resource_group.rg_materials.location
+#   resource_group_name = azurerm_resource_group.rg_materials.name
+#   application_type    = "web"
+#   workspace_id        = azurerm_log_analytics_workspace.law_materials.id
+#   retention_in_days   = 30
+#   tags                = local.common_tags
+# }
