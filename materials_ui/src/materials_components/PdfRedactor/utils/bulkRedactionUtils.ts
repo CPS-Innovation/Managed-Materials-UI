0db5a8b9b29e174d6178ef7e3780a50270da0884
benrome-cps <benjamin.rome@cps.gov.uk>
2026-06-29 15:07:19 +0000

    improve highlight accuracy

--- materials_ui/src/materials_components/PdfRedactor/utils/bulkRedactionUtils.ts
+++ materials_ui/src/materials_components/PdfRedactor/utils/bulkRedactionUtils.ts
@@ -2,23 +2,26 @@ import type { TBulkSearchResponse } from '../../CaseworkPdfRedactorWrapper/utils
 import type { TRedaction } from './coordUtils';
 import type { TSearchHighlight } from './searchHighlightUtils';
 
-const PADDING_INCHES = 0.03;
+const PADDING_X_INCHES = 0.03;
+const PADDING_Y_INCHES = 0.015;
 
 export const convertSearchResponseToRedactions = (
   resp: TBulkSearchResponse
 ): TRedaction[] =>
   resp.redactionDefinitions
     .flatMap((def) =>
-      def.redactionCoordinates.map((c) => ({
-        id: crypto.randomUUID(),
-        pageNumber: def.pageIndex,
-        pageWidth: def.width,
-        pageHeight: def.height,
-        x1: c.x1,
-        y1: def.height - c.y1,
-        x2: c.x2,
-        y2: def.height - c.y2
-      }))
+      def.redactionCoordinates.map((c) => {
+        return {
+          id: crypto.randomUUID(),
+          pageNumber: def.pageIndex,
+          pageWidth: def.width,
+          pageHeight: def.height,
+          x1: Math.min(c.x1, c.x2) + PADDING_X_INCHES,
+          y1: def.height - Math.min(c.y1, c.y2) - PADDING_Y_INCHES,
+          x2: Math.max(c.x1, c.x2) + PADDING_X_INCHES,
+          y2: def.height - Math.max(c.y1, c.y2) + PADDING_Y_INCHES
+        };
+      })
     )
     .sort((a, b) => {
       if (a.pageNumber !== b.pageNumber) return a.pageNumber - b.pageNumber;
@@ -33,8 +36,8 @@ export const convertCandidatesToSearchHighlights = (
     pageNumber: c.pageNumber,
     pageHeight: c.pageHeight,
     pageWidth: c.pageWidth,
-    xLeft: Math.min(c.x1, c.x2) + PADDING_INCHES,
-    xRight: Math.max(c.x1, c.x2) + PADDING_INCHES,
-    yTop: c.pageHeight - Math.max(c.y1, c.y2) - PADDING_INCHES,
-    yBottom: c.pageHeight - Math.min(c.y1, c.y2) + PADDING_INCHES
+    xLeft: Math.min(c.x1, c.x2),
+    xRight: Math.max(c.x1, c.x2),
+    yTop: c.pageHeight - Math.max(c.y1, c.y2),
+    yBottom: c.pageHeight - Math.min(c.y1, c.y2)
   }));
