d4560e3f19bbe0550032c7dadedcf1d947ac9bb5
Tahmeed Ahmed (NTT) <Tahmeed.Ahmed@cps.gov.uk>
2026-06-30 09:18:15 +0100

    error message amended

--- materials_ui/src/components/DocumentPreview/DocumentPreview.tsx
+++ materials_ui/src/components/DocumentPreview/DocumentPreview.tsx
@@ -1,9 +1,9 @@
+import { Banner } from '../../components';
 import { useDocumentPreview } from '../../hooks/';
 import { CaseMaterialsType } from '../../schemas/caseMaterials';
 import { ErrorSummary } from '../ErrorSummary/ErrorSummary';
 import { LoadingSpinner } from '../LoadingSpinner/LoadingSpinner';
 import { PdfViewer } from '../PdfViewer/PdfViewer';
-import { Banner } from '../../components';
 
 type Props = { row: CaseMaterialsType };
 
@@ -14,36 +14,25 @@ export default function DocumentPreview({ row }: Props) {
     error: caseDocumentError
   } = useDocumentPreview({ materialId: row.materialId });
 
-  const errorTitle = caseDocumentError?.toString().includes('403') ?
-    'This document is password protected' : 'No preview available';
+  const errorTitle = caseDocumentError?.toString().includes('403')
+    ? 'This document is password protected'
+    : 'There is a problem';
 
-  const errorMessage = caseDocumentError?.toString().includes('403') ?
-    'Ask the agency who supplied it to remove the password and resend the document.' :
-    'The material you are trying to preview is not currently available. You can view it in CMS.';
+  const errorMessage = caseDocumentError?.toString().includes('403')
+    ? 'Ask the agency who supplied it to remove the password and resend the document.'
+    : 'This document cannot be shown. You can still view it in CMS.';
 
   let content = null;
 
   if (!caseDocumentLoading) {
     if (caseDocumentError) {
       content = caseDocumentError.toString().includes('403') ? (
-        <Banner
-          type="error"
-          header={errorTitle}
-          content={errorMessage}
-        />
+        <Banner type="error" header={errorTitle} content={errorMessage} />
       ) : (
-        <ErrorSummary
-          errorTitle={errorTitle}
-          errorMessage={errorMessage}
-        />
+        <ErrorSummary errorTitle={errorTitle} errorMessage={errorMessage} />
       );
     } else if (caseDocumentData) {
-      content = (
-        <PdfViewer
-          file={caseDocumentData}
-          fileName={row.subject}
-        />
-      );
+      content = <PdfViewer file={caseDocumentData} fileName={row.subject} />;
     }
   }
 
