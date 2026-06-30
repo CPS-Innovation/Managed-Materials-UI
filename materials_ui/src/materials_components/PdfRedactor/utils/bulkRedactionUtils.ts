403b174f795806c25494c959e4ace90759874a1e
benrome-cps <benjamin.rome@cps.gov.uk>
2026-06-30 11:59:52 +0100

    improve spacing + change units to points

--- materials_ui/src/materials_components/PdfRedactor/utils/bulkRedactionUtils.ts
+++ materials_ui/src/materials_components/PdfRedactor/utils/bulkRedactionUtils.ts
@@ -2,8 +2,10 @@ import type { TBulkSearchResponse } from '../../CaseworkPdfRedactorWrapper/utils
 import type { TRedaction } from './coordUtils';
 import type { TSearchHighlight } from './searchHighlightUtils';
 
-const PADDING_X_INCHES = 0.03;
-const PADDING_Y_INCHES = 0.015;
+const POINTS_PER_INCH = 72;
+
+const PADDING_X_POINTS = 3;
+const PADDING_Y_POINTS = 1.1;
 
 export const convertSearchResponseToRedactions = (
   resp: TBulkSearchResponse
@@ -11,15 +13,16 @@ export const convertSearchResponseToRedactions = (
   resp.redactionDefinitions
     .flatMap((def) =>
       def.redactionCoordinates.map((c) => {
+        const toPoints = (inches: number) => inches * POINTS_PER_INCH;
         return {
           id: crypto.randomUUID(),
           pageNumber: def.pageIndex,
-          pageWidth: def.width,
-          pageHeight: def.height,
-          x1: Math.min(c.x1, c.x2) + PADDING_X_INCHES,
-          y1: def.height - Math.min(c.y1, c.y2) - PADDING_Y_INCHES,
-          x2: Math.max(c.x1, c.x2) + PADDING_X_INCHES,
-          y2: def.height - Math.max(c.y1, c.y2) + PADDING_Y_INCHES
+          pageWidth: toPoints(def.width),
+          pageHeight: toPoints(def.height),
+          x1: toPoints(Math.min(c.x1, c.x2)),
+          y1: toPoints(def.height - Math.max(c.y1, c.y2)) - PADDING_Y_POINTS,
+          x2: toPoints(Math.max(c.x1, c.x2)) + PADDING_X_POINTS,
+          y2: toPoints(def.height - Math.min(c.y1, c.y2)) + PADDING_Y_POINTS
         };
       })
     )
