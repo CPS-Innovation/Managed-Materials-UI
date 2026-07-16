2b30441eaf9ab82b61e8bdeeb811593490a80773
robmolloy-cps <robert.molloy@cps.gov.uk>
2026-07-16 09:02:34 +0000

    FCT2-20395-cwa-make-supporting-notes-required-where-redaction-type-other-is-selected-2 (#451)

--- materials_ui/src/materials_components/RedactionLog/RedactionLogModalBody.tsx
+++ materials_ui/src/materials_components/RedactionLog/RedactionLogModalBody.tsx
@@ -90,6 +90,8 @@ export const RedactionLogModalBody = ({
     formState: { errors },
   } = useFormContext<RedactionLogFormValues>();
 
+  const isSelectedRedactionOfOtherType = selectedRedactionTypes?.some((x) => x.name === 'Other');
+
   const [showPopover, setShowPopover] = useState(false);
 
   const underRedactionSelected = watch('underRedactionSelected');
@@ -339,7 +341,7 @@ export const RedactionLogModalBody = ({
           }}
         >
           <label className="govuk-label" htmlFor="supportingNotes">
-            Supporting notes (optional)
+            Supporting notes {isSelectedRedactionOfOtherType ? '' : '(optional)'}
           </label>
           {errors.supportingNotes && (
             <p className="govuk-error-message">{errors.supportingNotes.message}</p>
@@ -373,13 +375,22 @@ export const RedactionLogModalBody = ({
           )}
         </div>
 
+        {errors.supportingNotes && (
+          <p className="govuk-error-message">{errors.supportingNotes.message}</p>
+        )}
+
         <textarea
           className="govuk-textarea"
           id="supportingNotes"
           rows={5}
-          style={{ width: '50%' }}
+          style={{ width: '550px' }}
           maxLength={400}
           {...register('supportingNotes', {
+            validate: (value) => {
+              if (isSelectedRedactionOfOtherType && value.trim().length === 0)
+                return 'Please enter required Supporting notes';
+              return true;
+            },
             maxLength: { value: 400, message: 'Supporting notes cannot exceed 400 characters' },
           })}
         />
