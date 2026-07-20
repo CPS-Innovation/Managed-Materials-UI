9d91d1ec7d13b64ed98d012448291940ce1fb664
robmolloy-cps <robert.molloy@cps.gov.uk>
2026-07-20 15:36:03 +0100

    FCT2-16304-review-redact-tab-redaction-user-alerts-redaction-exceptio… (#455)
    * FCT2-16304-review-redact-tab-redaction-user-alerts-redaction-exceptions-update-1-3
    
    * FCT2-16304-review-redact-tab-redaction-user-alerts-redaction-exceptions-update-1-3

--- materials_ui/src/materials_components/RedactionLog/RedactionLogModalBody.tsx
+++ materials_ui/src/materials_components/RedactionLog/RedactionLogModalBody.tsx
@@ -341,8 +341,9 @@ export const RedactionLogModalBody = ({
           }}
         >
           <label className="govuk-label" htmlFor="supportingNotes">
-            Supporting notes {isSelectedRedactionOfOtherType ? '' : '(optional)'}
+            Supporting notes {isSelectedRedactionOfOtherType ? '(optional)' : ''}
           </label>
+
           {errors.supportingNotes && (
             <p className="govuk-error-message">{errors.supportingNotes.message}</p>
           )}
