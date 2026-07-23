f2225bb438034395e69f104e558d5c4444cd3efc
benrome-cps <benjamin.rome@cps.gov.uk>
2026-07-23 10:42:41 +0000

    remoev useEffect for closing modal

--- materials_ui/src/materials_components/RedactionLog/Modal.tsx
+++ materials_ui/src/materials_components/RedactionLog/Modal.tsx
@@ -11,14 +11,6 @@ export const Modal = ({ children, onClose, ariaLabel }: ModalProps) => {
   useLastFocus();
 
   useEffect(() => {
-    const onKeyDown = (e: KeyboardEvent) => {
-      if (e.code === 'Escape') onClose();
-    };
-    window.addEventListener('keydown', onKeyDown);
-    return () => window.removeEventListener('keydown', onKeyDown);
-  }, [onClose]);
-
-  useEffect(() => {
     document.body.style.overflow = 'hidden';
     return () => {
       document.body.style.overflow = '';
