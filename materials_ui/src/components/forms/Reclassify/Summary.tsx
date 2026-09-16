741e469aab4e19c66b62829c8101285e2817a15f
robmolloy-cps <robert.molloy@cps.gov.uk>
2026-09-16 16:48:15 +0000

    RMAT-129-removal-remove-add-witness-not-on-the-list-option-from-reclassification-flow-hk

--- materials_ui/src/components/forms/Reclassify/Summary.tsx
+++ materials_ui/src/components/forms/Reclassify/Summary.tsx
@@ -66,29 +66,27 @@ export const Summary = ({ data, onChange, onSave }: Props) => {
       />
 
       {data?.classification === 'STATEMENT' && (
-        <>
-          <SummaryCard
-            action={() => handleChangeClick('classification')}
-            title={`${documentType?.name} details`}
-            content={[
-              { key: 'Who is the Witness', value: witnessName || 'New witness (see below)' },
-              {
-                key: 'Does the statement have a date?',
-                value: data?.hasStatementDate ? 'Yes' : 'No',
-              },
-              ...(data?.hasStatementDate
-                ? [
-                    {
-                      key: 'What is the statement date?',
-                      value: data?.statementDate ? formatDate(data?.statementDate) : '',
-                    },
-                  ]
-                : []),
-              { key: 'Statement number', value: data?.statementNumber as number },
-              { key: 'What is the material status?', value: usedStatus },
-            ]}
-          />
-        </>
+        <SummaryCard
+          action={() => handleChangeClick('classification')}
+          title={`${documentType?.name} details`}
+          content={[
+            { key: 'Who is the Witness', value: witnessName || 'New witness (see below)' },
+            {
+              key: 'Does the statement have a date?',
+              value: data?.hasStatementDate ? 'Yes' : 'No',
+            },
+            ...(data?.hasStatementDate
+              ? [
+                  {
+                    key: 'What is the statement date?',
+                    value: data?.statementDate ? formatDate(data?.statementDate) : '',
+                  },
+                ]
+              : []),
+            { key: 'Statement number', value: data?.statementNumber as number },
+            { key: 'What is the material status?', value: usedStatus },
+          ]}
+        />
       )}
 
       {data?.classification === 'EXHIBIT' && (
